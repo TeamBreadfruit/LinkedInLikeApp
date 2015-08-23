a3e7795761f68031c7fec8d068f333d5d872b508
Veronika <VeronikaMartinova@users.noreply.github.com>
2015-08-23 17:35:19 +0300

    changed logins with username

--- LinkedInLikeApp/LinkedIn.Services/Controllers/AccountController.cs
+++ LinkedInLikeApp/LinkedIn.Services/Controllers/AccountController.cs
@@ -328,7 +328,7 @@ namespace LinkedIn.Services.Controllers
                 return BadRequest(ModelState);
             }
 
-            var user = new ApplicationUser() { UserName = model.Email, Email = model.Email };
+            var user = new ApplicationUser() { UserName = model.Username, Email = model.Email };
 
             IdentityResult result = await UserManager.CreateAsync(user, model.Password);
 
