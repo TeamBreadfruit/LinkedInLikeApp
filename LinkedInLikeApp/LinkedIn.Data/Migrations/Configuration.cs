5f8c3891a09ccffbe3f3381e6d9fbfa17c4d6273
Veronika <VeronikaMartinova@users.noreply.github.com>
2015-08-29 17:26:14 +0300

    seed method for degrees

--- LinkedInLikeApp/LinkedIn.Data/Migrations/Configuration.cs
+++ LinkedInLikeApp/LinkedIn.Data/Migrations/Configuration.cs
@@ -4,6 +4,8 @@ namespace LinkedIn.Data.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using System.Collections.Generic;
+    using LinkedIn.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<LinkedIn.Data.LinkedInContext>
     {
